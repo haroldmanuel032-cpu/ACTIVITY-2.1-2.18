9e1fb4d1ed33ba312e26de23ddda3a8b8ee03412
haroldmanuel032-cpu <haroldmanuel032@gmail.com>
2026-09-04 10:36:53 +0800

    Activity 2.1 (Desiderata)

---
+++ desiderata.java
@@ -0,0 +1,8 @@
+public class desiderata {
+  public static void main(String[] args) {
+    System.out.println("be on good terms with all persons. Speak your truth quietly and");
+    System.out.println("clearly; and listen to others, even to the dull and ignorant,");
+    System.out.println("they have to their story.");
+    System.out.println("Avoid loud and agressive person.");
+  }
+}
